535af1d6f0bdcb543bda1dc608c916c510e9a62a
Nicolas Balss <info@nicooler.de>
2014-04-07 10:45:05 +0200

    Datenbank Fehlerkorrektur

--- Datenbank/CreateTable.sql
+++ Datenbank/CreateTable.sql
@@ -1,28 +1,47 @@
-﻿CREATE DATABASE  IF NOT EXISTS `dienstplan` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
+﻿CREATE DATABASE  IF NOT EXISTS `dienstplan` /*!40100 DEFAULT CHARACTER SET utf8  */;
 USE `dienstplan`;
 
+/*  Das Hier oben nicht auf der offiziellen Seite benutzen!/**/
+
 drop table IF EXISTS Account;
+
 drop table IF EXISTS Mitarbeiter;
+
 
 create Table Mitarbeiter(
 MA_Id Int NOT NULL Auto_Increment, 
+
 Vorname varchar (40) Not Null, 
+
 Nachname varchar(40) Not Null, 
+
 GebDatum date Not Null, 
+
 Strasse varchar(45) Not Null, 
+
 Ort varchar(45) Not Null, 
+
 TelefonNr varchar (30) Not Null, 
+
 Beschaeftigungsgrad varchar (45) Not null, 
-Primary Key (MA_Id)
+
+Primary Key (MA_Id),
+
 Benutzername varchar (40) Not Null,
+
 Passwort varchar(40) Not Null,
-Rolle int()
+
+Rolle int(4)
+
 /*
+
 Rolle:0 = Admin
+
 Rolle:1 = Personalleitung
+
 Rolle:2 = Mitarbeiter
 /**/
+
 );
 
 
