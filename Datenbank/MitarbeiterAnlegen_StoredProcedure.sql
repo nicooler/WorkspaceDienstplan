4607b173a32a85733afb1b23ecf5a33fd8e7282f
Dominik <dom415135@hotmail.com>
2014-04-09 19:56:26 +0200

    Überarbeitung der stored procedures

--- Datenbank/MitarbeiterAnlegen_StoredProcedure.sql
+++ Datenbank/MitarbeiterAnlegen_StoredProcedure.sql
@@ -9,13 +9,16 @@ gebDatum date,
 straße varchar(50),
 ort varchar(50),
 telefonNr varchar(30),
-beschäftigungsgrad varchar(45)
+beschäftigungsgrad varchar(45),
+benutzername varchar (40),
+passwort varchar (40),
+rolle int (4)
 
 )
 BEGIN
 
 
-insert into Mitarbeiter (Vorname,Nachname,GebDatum,Straße,Ort,TelefonNr,Beschäftigungsgrad) VALUES (vorname,nachname,gebDatum,straße,ort,telefonNr,beschäftigungsgrad);
+insert into Mitarbeiter (Vorname,Nachname,GebDatum,Straße,Ort,TelefonNr,Beschäftigungsgrad, Benutzername, Passwort, Rolle) VALUES (vorname,nachname,gebDatum,straße,ort,telefonNr,beschäftigungsgrad, benutzername, passwort, rolle);
 
 
 
