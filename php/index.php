3932bb351b09e14d2e77f5f7f8ccdbc132bc36b6
Trooper628 <florian.maxludwig@web.de>
2014-04-24 12:51:11 +0200

    css

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!--Autor: Florian Baumann, Version 1, Userstory: Tabelle für PHP-->
+<!--Session Start und Anbindung an den header-->
+<?php
+session_start();
+include('header.php');
+?>
+<!-- Hier kann die Seite mit Inhalt gefüllt werden -->
+<tr >
+<td>
+Mit Inhalt füllen.
+</td>
+</tr>
+
+</body>
+</html>
